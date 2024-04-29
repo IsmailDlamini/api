112bcb71ff09cf3c8f99ed00626c039d86ad9a07
Ismail <68245583+IsmailDlamini@users.noreply.github.com>
2024-04-29 10:11:45 +0200

    Update index.js

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use("/api/About", AboutRoutes);
 app.use("/api/Users", userRoute);
 
 app.get("/", (req, res) => {
-  res.send("This is Ismail Dlamini's API and it is Running a portfolio and karabo and loyiso");
+  res.send("This is Ismail Dlamini's API and it is Running a portfolio SITE");
 });
 
 const mg = mailgun({
